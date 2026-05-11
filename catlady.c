8067b33b4879083ffeee41da2a6884b5631a410f
Justin Su <justinsucq@gmail.com>
2026-05-12 02:24:26 +0300

    first commit added variables

---
+++ catlady.c
@@ -0,0 +1,19 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <errno.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#define MAX_BOWLS 64
