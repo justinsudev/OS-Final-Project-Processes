df473a3cb930249ce0ef7434f380b5e0a98c0898
jasonj-lee <93573828+jasonj-lee@users.noreply.github.com>
2026-05-17 10:04:07 +0200

    reworking config to match processes version

--- cat-threads.c
+++ cat-threads.c
@@ -1,53 +1,36 @@
 #include <pthread.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#define NUM_CATS   3
-#define NUM_MICE   5
-#define NUM_BOWLS  4
-#define CYCLES     5
-
-typedef enum {
-    BOWL_FREE = 0,
-    BOWL_CAT,
-    BOWL_MOUSE
-} BowlState;
+#define MAX_BOWLS 64
 
 typedef struct {
-    BowlState state;
-    int owner_id;
-} Bowl;
+    int B;
+    int C;
+    int M;
+    int F;
+    int N;
+    int Fm;
+    int Nm;
+    int T;
+} Config;
 
-static Bowl bowls[NUM_BOWLS];
+typedef struct {
+    int B, C, M;
+    int F, N, Fm, Nm;
+    int bowls[MAX_BOWLS];
+    int active_cats;
+    int feeding_mice;
+    volatile sig_atomic_t stop;
+    struct timespec t0;
+} SharedState;
+
+static Config cfg;
+static SharedState *S = NULL;
 
 static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t cats_cv = PTHREAD_COND_INITIALIZER;
-static pthread_cond_t mice_cv = PTHREAD_COND_INITIALIZER;
-
-static int cats_present = 0;
-
-static int rand_ms(int lo, int hi) {
-    return lo + rand() % (hi - lo + 1);
-}
-
-static void sleep_ms(int ms) {
-    usleep(ms * 1000);
-}
-
-static int find_noncat_bowl(void) {
-    for (int i = 0; i < NUM_BOWLS; i++) {
-        if (bowls[i].state != BOWL_CAT) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-static int find_free_bowl(void) {
-    for (int i = 0; i < NUM_BOWLS; i++) {
-        if (bowls[i].state == BOWL_FREE) {
-            return i;
-        }
-    }
-    return -1;
-}
+static pthread_cond_t cv_no_cats = PTHREAD_COND_INITIALIZER;
+static pthread_cond_t cv_bowl_free = PTHREAD_COND_INITIALIZER;
